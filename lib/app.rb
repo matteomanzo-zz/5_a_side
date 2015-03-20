c74bb161184e8d61d9877ab094b2482a96f8c607
matteomanzo <imatteo.manzo@gmail.com>
2015-03-20 16:16:37 +0000

    all tests passing

--- lib/app.rb
+++ lib/app.rb
@@ -24,6 +24,7 @@ class App
   def generate_teams(number = available_players.length)
     available_players_dup = @available_players.dup.shuffle!
       first_team = available_players_dup[0..(number/2 -1)]
+      second_team = available_players_dup[(number/2 -1)..-1]
       first_team.each{|player| @team_one << player if !@team_one.include?(player) && !@team_two.include?(player)}
       second_team.each{|player| @team_two << player if !@team_one.include?(player) && !@team_two.include?(player)}
   end
